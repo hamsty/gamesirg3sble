130115577602f0a7573f2fedcb4509e2f7c023ff
hamsty <tallesdesousacosta@gmail.com>
2019-05-11 22:37:32 -0300

    inicial commit

---
+++ src/gamesirg3s.cpp
@@ -0,0 +1,133 @@
+#include <BLEDevice.h>
+#include <Arduino.h>
+#include <math.h>
+#include <string.h>
+#include "gamesirg3s.h"
+
+using namespace std;
+
+static BLEAddress address("86:55:06:68:2D:E0");
+static BLEUUID serviceUUID = BLEUUID("00008650-0000-1000-8000-00805f9b34fb");
+static BLEUUID charUUID = BLEUUID("00008651-0000-1000-8000-00805f9b34fb");
+static boolean connected = false;
+
+JoystickClient::JoystickClient()
+{
+    doConnect = false;
+    connected = false;
+    JoystickClient::init("JoystickClient");
+    BLEScan *pBLEScan = JoystickClient::getScan();
+    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+    pBLEScan->setInterval(1349);
+    pBLEScan->setWindow(449);
+    pBLEScan->setActiveScan(true);
+}
+
+JoystickClient::~JoystickClient()
+{
+    delete myDevice;
+    delete bclient;
+    delete data;
+    delete pRemoteCharacteristic;
+}
+
+class JoystickClient::MyClientCallback : public BLEClientCallbacks
+{
+    void onConnect(BLEClient *pclient)
+    {
+    }
+
+    void onDisconnect(BLEClient *pclient)
+    {
+        connected = false;
+        Serial.println("onDisconnect");
+    }
+};
+
+bool JoystickClient::connectToServer()
+{
+    Serial.print("Forming a connection to ");
+    Serial.println(myDevice->getAddress().toString().c_str());
+
+    BLEClient *pClient = BLEDevice::createClient();
+    Serial.println(" - Created client");
+
+    pClient->setClientCallbacks(new MyClientCallback());
+
+    // Connect to the remove BLE Server.
+    pClient->connect(myDevice); // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+    Serial.println(" - Connected to server");
+
+    // Obtain a reference to the service we are after in the remote BLE server.
+    BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
+    if (pRemoteService == nullptr)
+    {
+        Serial.print("Failed to find our service UUID: ");
+        Serial.println(serviceUUID.toString().c_str());
+        pClient->disconnect();
+        return false;
+    }
+    Serial.println(" - Found our service");
+
+    // Obtain a reference to the characteristic in the service of the remote BLE server.
+    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+    if (pRemoteCharacteristic == nullptr)
+    {
+        Serial.print("Failed to find our characteristic UUID: ");
+        Serial.println(charUUID.toString().c_str());
+        pClient->disconnect();
+        return false;
+    }
+    Serial.println(" - Found our characteristic");
+    connected = true;
+}
+
+class JoystickClient::MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{
+    /**
+   * Called for each advertising BLE server.
+   */
+    void onResult(BLEAdvertisedDevice advertisedDevice)
+    {
+        Serial.print("BLE Advertised Device found: ");
+        Serial.println(advertisedDevice.toString().c_str());
+
+        // We have found a device, let us now see if it contains the service we are looking for.
+        if (advertisedDevice.getAddress().equals(address))
+        {
+
+            BLEDevice::getScan()->stop();
+            myDevice = new BLEAdvertisedDevice(advertisedDevice);
+
+        } // Found our server
+    }     // onResult
+};        // MyAdvertisedDeviceCallbacks
+
+bool JoystickClient::isFind(){
+    return doConnect;
+};
+
+bool JoystickClient::isConnected(){
+    return connected;
+};
+
+bool JoystickClient::update(){
+    if(connected){
+        pRemoteCharacteristic->readValue();
+        data = pRemoteCharacteristic->readRawData();
+    }
+};
+
+pair<int,int> JoystickClient::getXY(){
+    int x = data[L3_X] > 128 ? 1 : (data[L3_X] < 128 ? -1 : 0);
+    int y = data[L3_Y] > 128 ? -1 : (data[L3_Y] < 128 ? 1 : 0);
+    return {x,y};
+};
+
+bool JoystickClient::aPressed(){
+    return data[BUTTONS_PLAY]==1;
+};
+
+bool JoystickClient::isConnected(){
+    return data[BUTTONS_PLAY]==2;
+};
---
+++ src/gamesirg3s.h
@@ -0,0 +1,45 @@
+#include <BLEDevice.h>
+#include <Arduino.h>
+#include <math.h>
+#include <string.h>
+
+#define L3_X 2
+#define L3_Y 3
+#define R3_X 4
+#define R3_Y 5
+#define L2 6
+#define R2 7
+#define BUTTONS_PLAY 8
+#define BUTTONS_CENTER 9
+#define DIGITAL 10
+
+
+class JoystickClient : BLEDevice {
+    public:
+        JoystickClient();
+        ~JoystickClient();
+        bool isFind();
+        bool connectToServer();
+        bool isConnected();
+        bool update();
+        std::pair<int,int> getXY();
+        bool aPressed();
+        bool bPressed();
+
+    private:
+        static BLEAddress address;
+        static BLEUUID serviceUUID;
+        static BLEUUID charUUID;
+        static boolean doConnect;
+        static boolean connected;
+        static BLEAdvertisedDevice *myDevice;
+
+        BLEClient *bclient;
+
+        class MyClientCallback;
+        class MyAdvertisedDeviceCallbacks;
+        uint8_t *data;
+
+        BLERemoteCharacteristic *pRemoteCharacteristic;
+};
+
